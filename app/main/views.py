a4888958e6fd006531096283eb7683ea315f4590
johnmwangi <jonesmwas356@gmail.com>
2019-04-12 17:15:38 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -21,18 +21,18 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search',movie_name=search_movie))
     else:
        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-@app.route('/movie/<int:id>')
-
+@main.route('/movie/<int:id>')
 def movie(id):
-    movie=get_movie(id)
+    movie = get_movie(id)
     title = f'{movie.title}'
-    return render_template('movie.html',title = title,movie = movie)
+    reviews = Review.get_reviews(movie.id)
+    return render_template('movie.html',title = title,movie = movie, reviews = reviews)
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
 
     movie_name_list = movie_name.split(" ")
@@ -45,12 +45,13 @@ def search(movie_name):
 def new_review(id):
     form=ReviewForm()
     movie=get_movie(id)
+
     if form.validate_on_submit():
         title=form.title.data
         review=form.review.data
         new_review=Review(movie.id,title,movie.poster,review)
         new_review.save_review()
-
         return redirect(url_for('main.movie',id=movie.id))
+
     title=f"{movie.title} review"
     return render_template('new_review.html',title=title,review_form=form,movie=movie)
