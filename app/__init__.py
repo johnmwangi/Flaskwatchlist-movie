d041b0b5aee1c2109c6653cb81047e47f78957d4
johnmwangi <jonesmwas356@gmail.com>
2019-04-12 14:42:30 +0300

    changes made

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+#from flask_sqlalchemy import SQLA1chemy
+# Initializing application
+# bootstrap = Bootstrap()
+#
+# def create_app(config_name):
+#
+app = Flask(__name__)
+
+
+bootstrap=Bootstrap(app)
+
+def create_app(config_name):
+    app.config.from_object(config_options[config_name])
+    #....
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
---
+++ start.sh
@@ -0,0 +1,3 @@
+MOVIE_API_KEY='e4a041a8b1ef6d85b87c90348756f621'
+SECRET_KEY='SECRET KEY'
+python3.6 manage.py server
